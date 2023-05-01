2e67173cbac0c3b503f51bf99048cacb184c6e06
Umar Moaviya <uk73745@gmail.com>
2023-04-30 22:13:10 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>You're ready to work!</h1>
+      <li className='fa fa-facebook fab'></li>
+    </div>
+  )
+}
+
+export default App
